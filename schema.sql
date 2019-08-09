836f89b377d7305dfbf0bf441d632c712abe8399
charlescortez <charlesmph@gmail.com>
2019-08-09 10:56:33 -0400

    initial commit added schema.sql

---
+++ schema.sql
@@ -0,0 +1,6 @@
+create table coffee {
+    id serial PRIMARY KEY,
+    name VARCHAR(200),
+    besoke VARCHAR(200),
+    size VARCHAR(200)
+};
---
+++ seed.sql
@@ -0,0 +1,7 @@
+insert into coffee
+    (name, bespoke, size)
+VALUES
+    ('Amy', 'Coffee Black', 'Grande'),
+    ('Andy', 'Americano Extra Cream Extra Sugar', 'Venti'),
+    ('Arethra', 'Cappuccino Light Foam', 'Tall'),
+    ('Alex', 'Breve Latte Extra Sugar', 'Extra Grande'),
